4630f8d9d89dbf4b0855b9ab0b35d506e4971bd1
tcarroll2 <tc98207@gmail.com>
2020-04-26 12:09:54 -0500

    Initial Commit

---
+++ Project13-tc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project13-tc
+//
+//  Created by Thomas Carroll on 4/26/20.
+//  Copyright © 2020 Thomas Carroll. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
